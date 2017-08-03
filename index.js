1a4fc793d64ff9e78249051f373eaedd63527a88
Al Ilseman <ilsemanal@johndeere.com>
2017-08-03 17:38:48 -0500

    linting

--- index.js
+++ index.js
@@ -1,86 +1,69 @@
 'use strict';
-
-console.log('Loading function');
-
 const doc = require('dynamodb-doc');
-
 const dynamo = new doc.DynamoDB();
-
 const TABLENAME = 'jrnl_entries';
 
+function _extractBody(event) {
+  var body = JSON.parse(event.body);
+  if (event.httpMethod === 'POST') {
+    return {
+      TableName: TABLENAME,
+      Item: body
+    };
+  }
+  return {
+    TableName: TABLENAME,
+    Key: { ID: body.ID }
+  };
 
-/**
- * Demonstrates a simple HTTP endpoint using API Gateway. You have full
- * access to the request and response payload, including headers and
- * status code.
- */
-exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    //some update
-
-    console.log("######~~CONSOLE LOG EVENT~~~######");
-    console.log(event);    
-    console.log("######~~CONSOLE LOG CONTEXT~~~######");
-    console.log(context);
-
-
-    const done = (err, res) => callback(null, {
-        statusCode: err ? '400' : '200',
-        body: err ? err.message : JSON.stringify(res),
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-        },
-    });
-
-    switch (event.httpMethod) {
-        case 'DELETE':
-            dynamo.deleteItem(extractBody(event), done);
-            break;
-        case 'GET':
-            if (event.queryStringParameters && event.queryStringParameters.ID) {
-                dynamo.getItem({
-                    TableName: TABLENAME,
-                    Key: {
-                        ID: event.queryStringParameters.ID
-                    }
-                }, done)
-            } else {
-                dynamo.scan({ TableName: TABLENAME }, done);                
-            }
-            break;
-        case 'POST':
-            var now = new Date();
-            var body = extractBody(event);
-
-            if (!body.Item.ID) { // If it's a new record
-                body.Item.ID = body.Item.Title + 'AT:' + now.toISOString();
-                body.Item.CreatedAt = now.toISOString();
-            } 
-
-            body.Item.UpdatedAt = now.toISOString();
-            
-            dynamo.putItem(body, done);
-            break;
-        default:
-            done(new Error(`Unsupported method "${event.httpMethod}"`));
-    }
-};
-
-function extractBody(event) {
-    var body = JSON.parse(event.body);
-    if (event.httpMethod === 'POST') {
-        return {
-            TableName: TABLENAME,
-            Item: body
-        }
-    } else {
-        return {
-            TableName: TABLENAME,
-            Key: {
-                ID: body.ID
-            }
-        }
-    }
 }
 
+
+function handler(event, context, callback) {
+  const now = new Date();
+  const body = _extractBody(event);
+
+  const done = (err, res) => {
+    return callback(null, {
+      statusCode: err ? '400' : '200',
+      body: err ? err.message : JSON.stringify(res),
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  };
+
+  switch (event.httpMethod) {
+    case 'DELETE': {
+      dynamo.deleteItem(body, done);
+      break;
+    }
+    case 'GET': {
+      if (event.queryStringParameters && event.queryStringParameters.ID) {
+        dynamo.getItem({
+          TableName: TABLENAME,
+          Key: { ID: event.queryStringParameters.ID }
+        }, done);
+      } else {
+        dynamo.scan({ TableName: TABLENAME }, done);
+      }
+      break;
+    }
+    case 'POST': {
+      if (!body.Item.ID) {
+        body.Item.ID = body.Item.Title + 'AT:' + now.toISOString();
+        body.Item.CreatedAt = now.toISOString();
+      }
+      body.Item.UpdatedAt = now.toISOString();
+      dynamo.putItem(body, done);
+      break;
+    }
+    default: {
+      done(new Error(`Unsupported method "${event.httpMethod}"`));
+    }
+  }
+}
+
+
+module.exports = { handler };
